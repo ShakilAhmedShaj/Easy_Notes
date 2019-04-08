a8c7dbf6d313f4cfd570a44b9cbee2746a9c3027
SHAJ <shakilahmedshaj@gmail.com>
2019-04-08 22:30:17 +0600

    lifecycle demos

--- app/src/main/java/com/t3ch/shaj/easynotes/NotesListActivity.java
+++ app/src/main/java/com/t3ch/shaj/easynotes/NotesListActivity.java
@@ -2,12 +2,55 @@ package com.t3ch.shaj.easynotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NotesListActivity extends AppCompatActivity {
+
+    private static final String TAG = "NotesListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+
+        Log.d(TAG, "onCreate: Done");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle", "onStart invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle", "onResume invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle", "onPause invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle", "onStop invoked");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifecycle", "onRestart invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle", "onDestroy invoked");
+    }
+
+
 }
