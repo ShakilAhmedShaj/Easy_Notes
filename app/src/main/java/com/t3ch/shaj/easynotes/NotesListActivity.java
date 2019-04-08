deb38fc7f419c3e9003cc31ec493375742555b3b
SHAJ <shakilahmedshaj@gmail.com>
2019-04-08 23:38:36 +0600

    (8) Data Model

--- app/src/main/java/com/t3ch/shaj/easynotes/NotesListActivity.java
+++ app/src/main/java/com/t3ch/shaj/easynotes/NotesListActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.t3ch.shaj.easynotes.models.Note;
+
 public class NotesListActivity extends AppCompatActivity {
 
     private static final String TAG = "NotesListActivity";
@@ -13,44 +15,9 @@ public class NotesListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
 
-        Log.d(TAG, "onCreate: Done");
-    }
+        Note note = new Note("some title","some content","time");
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("lifecycle", "onStart invoked");
+        Log.d(TAG, "onCreate: my note" + note.toString());
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("lifecycle", "onResume invoked");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("lifecycle", "onPause invoked");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("lifecycle", "onStop invoked");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("lifecycle", "onRestart invoked");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("lifecycle", "onDestroy invoked");
-    }
-
 
 }
