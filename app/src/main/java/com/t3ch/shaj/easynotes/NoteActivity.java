6a8eb4e9a8d8fc16476025c1e901b38a90d2d1ad
SHAJ <shakilahmedshaj@gmail.com>
2019-04-27 22:00:45 +0600

    (30) Handling Screen Rotations

--- app/src/main/java/com/t3ch/shaj/easynotes/NoteActivity.java
+++ app/src/main/java/com/t3ch/shaj/easynotes/NoteActivity.java
@@ -261,4 +261,19 @@ public class NoteActivity extends AppCompatActivity implements
             super.onBackPressed();
         }
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("mode", mMode);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mMode = savedInstanceState.getInt("mode");
+        if (mMode == EDIT_MODE_ENABLED) {
+            enableEditMode();
+        }
+    }
 }
