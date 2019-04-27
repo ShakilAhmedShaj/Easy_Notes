6611a9b0becd2fe281895e960b41c33ff0af6478
SHAJ <shakilahmedshaj@gmail.com>
2019-04-27 22:44:54 +0600

    (32) Entities added

--- app/src/main/java/com/t3ch/shaj/easynotes/models/Note.java
+++ app/src/main/java/com/t3ch/shaj/easynotes/models/Note.java
@@ -1,5 +1,9 @@
 package com.t3ch.shaj.easynotes.models;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
 import android.os.Parcel;
 import android.os.Parcelable;
 
@@ -7,10 +11,20 @@ import android.os.Parcelable;
  * Created by Shakil Ahmed Shaj on 08-Apr-19.
  * shakilahmedshaj@gmail.com
  */
+
+@Entity(tableName = "notes")
 public class Note implements Parcelable {
 
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "title")
     private String title;
+
+    @ColumnInfo(name = "content")
     private String content;
+
+    @ColumnInfo(name = "timestamp")
     private String timestamp;
 
     public Note(String title, String content, String timestamp) {
@@ -19,10 +33,13 @@ public class Note implements Parcelable {
         this.timestamp = timestamp;
     }
 
+    @Ignore
     public Note() {
     }
 
+
     protected Note(Parcel in) {
+        id = in.readInt();
         title = in.readString();
         content = in.readString();
         timestamp = in.readString();
@@ -39,6 +56,14 @@ public class Note implements Parcelable {
             return new Note[size];
         }
     };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
@@ -67,7 +92,8 @@ public class Note implements Parcelable {
     @Override
     public String toString() {
         return "Note{" +
-                "title='" + title + '\'' +
+                "id=" + id +
+                ", title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 ", timestamp='" + timestamp + '\'' +
                 '}';
@@ -80,8 +106,11 @@ public class Note implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
         dest.writeString(title);
         dest.writeString(content);
         dest.writeString(timestamp);
     }
+
+
 }
